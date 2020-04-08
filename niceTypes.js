65bf3f7ed64ec4632e8466be7a6627dcea37ada2
Yarin Shlafir <shlafiry@gmail.com>
2020-04-08 10:19:29 +0300

    All of niceTypes+nicerType+nicestTypes

--- niceTypes.js
+++ niceTypes.js
@@ -4,43 +4,56 @@
  * Author: YS
  * Date: 5.4.2020
  ****************************/
+const _ = require('lodash');
 
+/* This includes all of niceTypes+nicerTypes+nicestTypes, because I started with something
+ that nicest types includes. */
+
+function validateArrayType(arr, type) {
+    for (const element of arr) {
+        validateTypes([element], [type]);
+    }
+    return true;
+}
+
+/**
+ * Validates arguments' types according to the user's requested types
+ * @param {Array} args - the function's arguments (use arguments here)
+ * @param {Array} types - the user's requested types for validation, order is crucial, e.g Number, Function, etc.
+ * use undefined to skip over an argument's validation, meaning it can receive any type as an argument.
+ * use undefined to skip over an argument's validation, meaning it can receive any type as an argument.
+ */
 function validateTypes(args, types) {
     if (args.length !== types.length) {
         throw new Error(`You have not entered enough arguments, ${types.length} is expected`)
     }
+
     for (let i = 0; i < args.length; i++){
-        // Handeling primitve string
-        if(types[i] === String){
-            if(typeof(args[i]) === 'string') {
-                continue;
-            }
-        }
-
-        // Handeling primitve number
-        if(types[i] === Number){
-            if(typeof(args[i]) === 'number') {
-                continue;
-            }
-        }
-
-        // Handeling primitve boolean
-        if(types[i] === Boolean){
-            if(typeof(args[i]) === 'boolean') {
-                continue;
-            }
+        if ((_.isArray(types[i]) && validateArrayType(args[i], types[i][0])) ||
+            (types[i] === String && _.isString(args[i])) ||
+            (types[i] === Number && _.isNumber(args[i])) ||
+            (types[i] === Boolean && _.isBoolean(args[i])) ||
+            (types[i] === Object && _.isObject(args[i])) ||
+            (types[i] === Function && _.isFunction(args[i])) ||
+            (types[i] === Array && _.isArray(args[i])) ||
+            (types[i] === undefined)) {
+            continue;
         }
 
         if (!(args[i] instanceof types[i])) {
-            console.log(typeof(args[i]));
-            console.log((args[i] instanceof types[i]));
-            throw new Error(`The ${i+1} parameter is invalid, expected: ${types[i]}`);
+            throw new Error(`The ${i+1} parameter is invalid, expected: ${types[i].name}`);
         }
     }
 }
 
-function doNothing(name, age, employed) {
-    validateTypes(arguments, [String, Number, Boolean]);
+
+
+function doNothing(name, age, employed, obj, pir, any, func, arr, numarr) {
+    validateTypes(arguments, [String, Number, Boolean, Object, Pirate, undefined, Function, Array, [Number]]);
 }
 
-doNothing(12, 45, false);
+class Pirate {
+
+}
+
+doNothing('Build', 45, false, {'type':12, 'book':11}, new Pirate(), 'random', (x => x), ['q',2], [1, 5, 'a', 6]);
