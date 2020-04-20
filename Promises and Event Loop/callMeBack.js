6495ddc7e36122c8ce3a1f0bf2757481350442e3
Yarin Shlafir <shlafiry@gmail.com>
2020-04-20 12:42:42 +0300

    promiseMeBack is also here :)

--- Promises and Event Loop/callMeBack.js
+++ Promises and Event Loop/callMeBack.js
@@ -7,7 +7,7 @@
 function getDivisionBy2(num, func) {
     console.log(`The number ${num} passed to getDivisionBy2`);
     if (num % 2 === 0){
-        func(num/2);
+        func(num / 2);
     } else {
         throw new Error(`The number is not divisible by 2`);
     }
