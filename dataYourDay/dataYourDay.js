957634c4c6a88c84a645adef6faaecd016198ad9
Yarin Shlafir <shlafiry@gmail.com>
2020-04-26 14:53:35 +0300

    Beggining to fix

--- dataYourDay/dataYourDay.js
+++ dataYourDay/dataYourDay.js
@@ -7,29 +7,44 @@
 
 const fs = require('fs');
 
+const NO_SUCH_FILE = 'ENOENT';
+const DIARY_FILE = 'diary.json';
+const messages = {
+    NAME_REQUEST: 'What is your name?',
+    DAY_DESCRIPTION_REQUEST: 'How was your day?'
+};
+
+
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-fs.readFile('diary.json', (err, data) => {
+fs.readFile(DIARY_FILE, (err, data) => {
     let diary;
     if (err) {
         // if the file does not exist, we will initialize a new diary
-        if (err.code === 'ENOENT') {
-            diary = {entries: []};
-        } else throw err;
-    } else diary = JSON.parse(data.toString());
+        if (err.code === NO_SUCH_FILE) {
+            diary = [];
+        } else {
+            throw err;
+        }
+    } else {
+        diary = JSON.parse(data.toString());
+    }
 
-    readline.question('What is your name?', function (name) {
-        readline.question('How was your day?', function (text) {
-            let entry = {
+    readline.question(messages.NAME_REQUEST, function (name) {
+        readline.question(messages.DAY_DESCRIPTION_REQUEST, function (text) {
+            const entry = {
                 name: name,
                 text: text
             };
-            diary.entries.push(entry);
-            fs.writeFile('diary.json', JSON.stringify(diary, null, 2), err => {
-                if (err) throw err;
+            diary.push(entry);
+            fs.writeFile(DIARY_FILE, JSON.stringify(diary, null, 2), err => {
+                if (err) {
+                    readline.close();
+                    throw err;
+                }
                 readline.close();
             });
         });
