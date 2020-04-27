369e688a591b4fe62cb1e1350e405a1171f0f7fc
Yarin Shlafir <shlafiry@gmail.com>
2020-04-27 14:53:10 +0300

    All is promise now

--- fileromise/fileromise.js
+++ fileromise/fileromise.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 function wrapReadFile(fileName) {
-    return new Promise(((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         fs.readFile(fileName, (err, data) => {
             if (err) {
                 reject(err);
@@ -10,21 +10,41 @@ function wrapReadFile(fileName) {
                 resolve(data);
             }
         });
-    }));
+    });
 }
 
 function wrapAppendFile(fileName, data) {
-    return new Promise(((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         fs.appendFile(fileName, data, (err) => {
-            if (err){
+            if (err) {
                 reject(err);
             } else {
                 resolve();
             }
         })
-    }));
+    });
 }
 
-async function wrapCopyFile(from) {
-    return fs.readFile.__promisify__(from);
+function wrapCopyFile(source, dest) {
+    return new Promise((resolve, reject) => {
+        fs.copyFile(source, dest, err => {
+           if (err) {
+               reject(err);
+           } else {
+               resolve();
+           }
+        });
+    });
+}
+
+function wrapReadDir(dir) {
+    return new Promise((resolve, reject) => {
+       fs.readdir(dir, (err, files) => {
+           if (err) {
+               reject(err);
+           } else {
+               resolve(files);
+           }
+       });
+    });
 }
